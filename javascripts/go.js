cf2aa61cf38c1a51a4a164d83df4a889e2997f24
Mathew Ostrander <mathew.ostrander@gmail.com>
2015-11-09 10:51:43 -0600

    files seemingly installed correctly

---
+++ javascripts/go.js
@@ -0,0 +1,21 @@
+requirejs.config({
+	baseUrl : "./javascripts/",
+	shim : {
+		bootstrap : {
+            deps : [ 'jquery'],
+            exports: 'bootstrap'
+        }
+	},
+	paths : {
+		"jquery": "../lib/bower_components/jquery/dist/jquery.min",
+		"hbs": "../lib/bower_components/require-handlebars-plugin/hbs",
+		"bootstrap": "../lib/bower_components/bootstrap/dist/js/bootstrap.min"
+	}
+});
+
+require(["hbs"],
+	function(Handlebars) {
+
+
+	}
+);
