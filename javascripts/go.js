0590c0834af886267fa2a5802a28c2e87d6fd9cf
Mathew Ostrander <mathew.ostrander@gmail.com>
2015-11-10 17:59:11 -0600

    trying to get bootstrap-selector plugin to work with require

--- javascripts/go.js
+++ javascripts/go.js
@@ -4,17 +4,22 @@ requirejs.config({
 		bootstrap : {
             deps : [ 'jquery'],
             exports: 'bootstrap'
+        },
+        "bootstrap-select" : {
+            deps : [ 'jquery']
         }
 	},
 	paths : {
 		"jquery": "../lib/bower_components/jquery/dist/jquery.min",
 		"hbs": "../lib/bower_components/require-handlebars-plugin/hbs",
-		"bootstrap": "../lib/bower_components/bootstrap/dist/js/bootstrap.min"
+		"bootstrap": "../lib/bower_components/bootstrap/dist/js/bootstrap.min",
+		"bootstrap-select": "../lib/bower_components/bootstrap-select/dist/js/bootstrap-select"
 	}
 });
 
-require(["jquery", "bootstrap", "hbs", "phone", "dice"],
-	function($, bootstrap, Handlebars, phone, dice) {
+require(["jquery", "bootstrap", "bootstrap-select", "hbs", "phone", "dice"],
+	function($, bootstrap, blect, Handlebars, phone, dice) {
+	$('.selectpicker').selectpicker();
 
 
 	}
