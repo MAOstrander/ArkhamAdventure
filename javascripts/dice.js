2fa8806cb8f7e50384450443b3de6462f77d72c6
Mathew Ostrander <mathew.ostrander@gmail.com>
2015-11-11 09:28:15 -0600

    player select for limits added

--- javascripts/dice.js
+++ javascripts/dice.js
@@ -31,6 +31,14 @@ define(["jquery"], function($) {
 		return successCount;
 	}
 
+	function printOutput(rolls, rollResults, state) {
+		var output = "<div>You rolled " + rolls.length + " dice.</div>";
+		output += "<div>You got: " + rolls + "</div>";
+		output += "<div>Since you were " + state + " that resulted in " + rollResults +" successes</div>";
+
+		$("#results").html(output);
+	}
+
 	$("#dice").click(function(){
 		var numDice = $("[name='diceNumber']").val();
 		var state = $("[name='state']").val();
@@ -38,10 +46,41 @@ define(["jquery"], function($) {
 		console.log("Roll of the dice:", rolls);
 		var rollResults = checkSuccess(rolls, state);
 		console.log("Roll Results: ", rollResults);
+		printOutput(rolls, rollResults, state);
 	});
+
+	$("button[type='button']").click(function(event){
+		var players = $(this).val();
+		players = parseInt(players);
+		var gates = 5;
+		switch(players) {
+    		case 1:
+    		case 2:
+        		gates = 8;
+        	break;
+    		case 3:
+    		case 4:
+        		gates = 7;
+        	break;
+    		case 5:
+    		case 6:
+        		gates = 6;
+        	break;
+    		case 7:
+    		case 8:
+        		gates = 5;
+        	break;
+		}
+	
+		$("#monster-limit").html(players + " Player Monster Limit: " + (3+players));
+		$("#outskirt-limit").html(players + " Player Outskirt Limit: " + (8-players));
+		$("#gate-limit").html(players + " Player Open Gate Limit: " + gates);
+	});
+
 	return {
 		rollD6:rollD6,
-		checkSuccess: checkSuccess
+		checkSuccess: checkSuccess,
+		printOutput: printOutput
   };
 
 }); //END DEFINE FUNCTION
